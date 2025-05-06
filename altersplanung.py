d127d642b08152cf64fd555cc8a4befe551e8540
Vlad <voland.gordienko@gmail.com>
2025-05-06 11:18:24 +0200

    Altersplanung

--- altersplanung.py
+++ altersplanung.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import time
+import json
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut
 
@@ -8,6 +9,7 @@ FILENAME = "Altersplanung_Anbieterverzeichnis.xlsx"
 INPUT_DIR = "data"
 OUTPUT_DIR = "result"
 SHEET_NAME = "Anbieter"
+CACHE_FILE = "geocode_cache.json"
 
 INPUT_PATH = os.path.join(INPUT_DIR, FILENAME)
 OUTPUT_PATH = os.path.join(OUTPUT_DIR, "anbieter_mit_koordinaten.xlsx")
@@ -15,10 +17,13 @@ OUTPUT_PATH = os.path.join(OUTPUT_DIR, "anbieter_mit_koordinaten.xlsx")
 geolocator = Nominatim(user_agent="superset-mapper")
 geocode_cache = {}
 
-def geocode_address(address):
+def geocode_address(address, index, length):
+    print(f"{index + 1}/{length}: {address}")
     if address in geocode_cache:
+        print('get from cache') # todo
         return geocode_cache[address]
     try:
+        print('get from geolocator')
         location = geolocator.geocode(address)
         if location:
             coords = (location.latitude, location.longitude)
@@ -26,10 +31,26 @@ def geocode_address(address):
             coords = (None, None)
     except GeocoderTimedOut:
         time.sleep(1)
-        return geocode_address(address)
+        return geocode_address(address, index, length)
     geocode_cache[address] = coords
-    time.sleep(1)  # пауза между запросами
+    save_geocode_cache()
+    time.sleep(1)
     return coords
+
+def save_geocode_cache():
+    existing_cache = {}
+
+    if os.path.exists(CACHE_FILE):
+       try:
+           with open(CACHE_FILE, "r", encoding="utf-8") as f:
+               existing_cache = json.load(f)
+       except json.JSONDecodeError:
+           print("geocode_cache.json corrupted or empty - the cache will be recreated.")
+
+       updated_cache = {**existing_cache, **geocode_cache}
+
+       with open(CACHE_FILE, "w", encoding="utf-8") as f:
+           json.dump(updated_cache, f, ensure_ascii=False, indent=2)
 
 def parse_adressen():
     os.makedirs(OUTPUT_DIR, exist_ok=True)
@@ -38,8 +59,22 @@ def parse_adressen():
         print(f"Error: File {INPUT_PATH} not found.")
         return
 
+    if os.path.exists(CACHE_FILE):
+        try:
+            with open(CACHE_FILE, "r", encoding="utf-8") as f:
+                geocode_cache = json.load(f)
+        except json.JSONDecodeError:
+            print("geocode_cache.json corrupted or empty - the cache will be recreated.")
+            geocode_cache = {}
+    else:
+        geocode_cache = {}
+
+    geocode_cache = {str(k): tuple(v) for k, v in geocode_cache.items()}
+
     df = pd.read_excel(INPUT_PATH, sheet_name=SHEET_NAME, dtype=str)
     df = df.dropna(subset=["Anschrift", "PLZ", "Ort"])
+    df = df.drop_duplicates(subset=["Anschrift", "PLZ", "Ort"])
+    df.reset_index(drop=True, inplace=True)
 
     df["Anschrift"] = df["Anschrift"].str.strip()
     df["PLZ"] = df["PLZ"].astype(str).str.strip()
@@ -49,10 +84,24 @@ def parse_adressen():
     df["latitude"] = None
     df["longitude"] = None
 
+    length = len(df)
     for i, row in df.iterrows():
-        coords = geocode_address(row["full_address"])
+        coords = geocode_address(row["full_address"], i, length)
         df.at[i, "latitude"] = coords[0]
         df.at[i, "longitude"] = coords[1]
+
+    columns_to_keep = [
+        "Anbieter",
+        "Anschrift",
+        "PLZ",
+        "Ort",
+        "Telefon",
+        "E-Mail",
+        "full_address",
+        "latitude",
+        "longitude"
+    ]
+    df = df[columns_to_keep]
 
     df.to_excel(OUTPUT_PATH, index=False)
     print(f"Result saved to {OUTPUT_PATH}")
